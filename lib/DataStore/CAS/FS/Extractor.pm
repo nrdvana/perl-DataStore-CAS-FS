ad9ce0831daebd7e302b875661f3ba3cb19db57d
Michael Conrad <mike@nrdvana.net>
2013-04-21 00:46:26 -0400

    Updated FS::Extractor to match new API
    Also did some belated obvious stuff like giving it a constructor...

--- lib/DataStore/CAS/FS/Extractor.pm
+++ lib/DataStore/CAS/FS/Extractor.pm
@@ -1,15 +1,34 @@
-package File::CAS::Extractor;
+package DataStore::CAS::FS::Extractor;
+use 5.008;
 use strict;
 use warnings;
+use Carp;
 
 use Unix::Mknod;
 use Fcntl ':mode';
 use POSIX;
 
+sub new {
+	my $class= shift;
+	my %p= ref($_[0])? %{$_[0]} : @_;
+	$class->_ctor(\%p);
+}
+
+our @_ctor_params= ();
+
+sub _ctor {
+	my ($class, $p)= @_;
+	my %self= map { $_ => delete $p->{$_} } @_ctor_params;
+	croak "Invalid param(s): ".join(', ', keys %$p)
+		if keys %$p;
+
+	bless \%self, $class;
+}
+
 sub extract {
-	my ($self, $cas, $entry, $destPath)= @_;
-	my $name= catfile($destPath,$entry->name);
-	my $hash= $entry->hash;
+	my ($self, $cas, $entry, $dest_path)= @_;
+	my $name= catfile($dest_path, $entry->name);
+	my $hash= $entry->ref;
 	if (($entry->type eq 'file' or $entry->type eq 'dir') and (!defined $hash or !length $hash)) {
 		warn "Skipping stub '$name'\n";
 		return;
@@ -18,28 +37,27 @@ sub extract {
 	if ($entry->type eq 'file') {
 		my $f= $cas->get($hash)
 			or die "Store is missing entry '$hash'\n";
-		open my $fh, '>:raw', $fnam
-			or die "open: $!";
-		my $buf;
-		while (my $n= $f->read($buf, 64*1024)) {
-			print $fh $buf;
-		}
-		close $fh or die "close: $!";
+		open(my $dest_fh, '>', $name) or die "open($name): $!\n";
+		my $src_fh= $f->open;
+		$self->_copy_fh($src_fh, $dest_fh);
+		close $src_fh or die "close: $!";
+		close $dest_fh or die "close: $!";
 	}
 	elsif ($entry->type eq 'dir') {
-		my $d= $cas->getDir($hash)
+		my $d= DataStore::CAS::FS::Dir->new(file => $cas->get($hash))
 			or die "Store is missing entry '$hash'\n";
-		for ($d->getEntries) {
-			$self->extract($cas, $_, $name);
+		my $iter= $d->iterator;
+		while (defined (my $ent= $iter->())) {
+			$self->extract($cas, $ent, $name);
 		}
 	}
 	elsif ($entry->type eq 'symlink') {
-		symlink $entry->symlinkTarget, $name
+		symlink $entry->ref, $name
 			or die "Failed to create symbolic link '$name': $!\n";
 	}
 	elsif ($entry->type eq 'blockdev' or $entry->type eq 'chardev') {
-		my ($major, $minor)= split /,/, $entry->device;
-		defined $major and defined $minor or die "Invalid device for $name: ".$entry->device."\n";
+		my ($major, $minor)= split /,/, $entry->ref;
+		defined $major and defined $minor or die "Invalid device for $name: ".$entry->ref."\n";
 		my $mode= $entry->unix_mode;
 		$mode= ($entry->type eq 'blockdev')? (S_IFBLK | 0660) : (S_IFCHR | 0660)
 			unless (defined $mode);
@@ -58,6 +76,15 @@ sub extract {
 	else {
 		warn "Skipping '$name' of type '".$entry->type."'\n";
 	}
+}
+
+sub _copy_fh {
+	my ($src, $dest)= @_;
+	my ($buf, $got);
+	while ($got= read($src, $buf, 1024*1024)) {
+		(print $dest, $buf) or die "write: $!\n";
+	}
+	defined $got or die "read: $!\n";
 }
 
 1;
